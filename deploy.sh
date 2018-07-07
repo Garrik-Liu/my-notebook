a44eae2c1a3d09e358794dba778b1b4c25465d53
Garrik-Liu <garrikliu@Gmail.com>
2018-07-06 23:26:16 -0400

    update

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,8 @@ git add *
 git commit -m "update"
 git push
 
+# 删除 ph-pages 分支，以防冲突。每次提交都是暴力覆盖
+git branch -d origin/ph-pages
+
+# 提交到远程
 git subtree push --prefix docs/.vuepress/dist origin gh-pages
