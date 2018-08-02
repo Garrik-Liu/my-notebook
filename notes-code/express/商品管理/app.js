42e423bc82074a6dda1455a4f2293420a55667db
Garrik-Liu <garrikliu@Gmail.com>
2018-08-02 12:55:11 -0400

    update

--- notes-code/express/商品管理/app.js
+++ notes-code/express/商品管理/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')("short");
 // 解析 HTTP 请求体
 const bodyParser = require('body-parser');
 // 操作 Mongo 数据库
-const MongoClient = require('mongodb').MongoClient;
+const db = require('./modules/db');
 // 操作 session
 const session = require('express-session');
 // 操作 cookie
@@ -18,7 +18,7 @@ const app = express();
 // 数据库相关变量
 const DbUrl = "mongodb://localhost:27017/"
 const TargetDb = "product-manage";
-const TargetCol = "users";
+
 
 // 用于 cookie 签名的 key
 const signKey = 'secret';
@@ -53,7 +53,8 @@ app.use(express.static('public'));
 
 // app.locals 用于将值传递到所渲染的模板中
 app.use((req, res, next) => {
-    app.locals['userinfo'] = {};
+    app.locals.userinfo = {};
+    app.locals.products = [];
     next();
 })
 
@@ -65,10 +66,8 @@ app.use((req, res, next) => {
 
         if (req.session.userinfo && req.session.userinfo.username != '') {
             // 将 session 登录信息保存到全局登录信息(userinfo)
-            app.locals['userinfo'] = req.session.userinfo;
-
+            app.locals.userinfo = req.session.userinfo;
         }
-
         next();
 
         // 如果请求 不是 访问登录页面(/login) 或 发送登录请求 (/dologin) 
@@ -77,7 +76,7 @@ app.use((req, res, next) => {
     } else {
         if (req.session.userinfo && req.session.userinfo.username != '') {
             // 将 session 登录信息保存到全局登录信息(userinfo)
-            app.locals['userinfo'] = req.session.userinfo;
+            app.locals.userinfo = req.session.userinfo;
 
             next();
         } else {
@@ -98,7 +97,46 @@ app.get('/login', (req, res) => {
 
 // 商品列表页面
 app.get('/product', (req, res) => {
-    res.render('product');
+
+    db.find(DbUrl, TargetDb, "product", {}, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+
+        res.render('product', {
+            products: result,
+        });
+    })
+})
+
+app.get('/product-edit', (req, res) => {
+    db.find(DbUrl, TargetDb, "product", { "_id": new db.ObjectID(req.query.id) }, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(data[0])
+            res.render('product-edit', {
+                action: '/doproduct-edit',
+                actionTitle: '修改',
+                item: {
+                    id: data[0]._id,
+                    name: data[0].name,
+                    price: data[0].price,
+                    quantity: data[0].quantity
+                },
+            });
+        }
+
+    })
+
+})
+
+app.get('/product-add', (req, res) => {
+    res.render('product-edit', {
+        action: '/doproduct-add',
+        actionTitle: '添加',
+        item: {},
+    });
 })
 
 // 以下是请求路由, 以上是页面路由
@@ -108,41 +146,29 @@ app.get('/product', (req, res) => {
 app.post('/dologin', (req, res) => {
     // 获得用户输入
     const userInput = req.body;
-
     // 链接数据库
-    MongoClient.connect(DbUrl, (err, db) => {
+    db.find(DbUrl, TargetDb, "users", { "username": userInput.username }, (err, result) => {
         if (err) {
             console.log(err);
         } else {
-            const dbase = db.db(TargetDb);
+            // 如果 length 大于 0, 意味着匹配到了数据
+            if (result.length) {
+                // 判断匹配到的密码 和 用户输入是否相同, 相同则把登录信息保存在 session.
+                if (result[0].password == md5(userInput.password)) {
+                    req.session.userinfo = result[0];
 
-            // 注: toArray() 返回 一个包含所有 匹配到的 documens 的数组.
-            dbase.collection(TargetCol).find({ "username": userInput.username }).toArray((err, result) => {
-                if (err) {
-                    console.log(err);
+                    // 定义相关登录状态
+                    console.log("登陆成功");
                 } else {
-                    // 如果 length 大于 0, 意味着匹配到了数据
-                    if (result.length) {
-                        // 判断匹配到的密码 和 用户输入是否相同, 相同则把登录信息保存在 session.
-                        if (result[0].password == md5(userInput.password)) {
-                            req.session.userinfo = result[0];
-
-                            // 定义相关登录状态
-                            console.log("登陆成功");
-                        } else {
-                            console.log("密码错误");
-                        }
-                    } else {
-                        console.log("用户名不存在");
-                    }
-                    db.close();
-
-                    // 跳转到主页. 如果登录成功会停留在主页, 登录失败会继续跳转到登录页
-                    res.redirect('/');
+                    console.log("密码错误");
                 }
-            });
+            } else {
+                console.log("用户名不存在");
+            }
+            // 跳转到主页. 如果登录成功会停留在主页, 登录失败会继续跳转到登录页
+            res.redirect('/');
         }
-    });
+    })
 });
 
 // 登出请求
@@ -159,21 +185,54 @@ app.get('/logout', (req, res) => {
 })
 
 // 添加商品请求
-app.get('/product-add', (req, res) => {
+app.post('/doproduct-add', (req, res) => {
+    let userInput = req.body;
 
-    res.send('product-add');
+    let obj = {
+        name: userInput.name,
+        price: userInput.price,
+        quantity: userInput.quantity,
+    }
+
+    db.add(DbUrl, TargetDb, "product", obj, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/product');
+        }
+    })
 })
 
 // 修改商品请求
-app.get('/product-edit', (req, res) => {
+app.post('/doproduct-edit', (req, res) => {
+    let userInput = req.body;
 
-    res.send('product-edit');
+    let obj = {
+        name: userInput.name,
+        price: userInput.price,
+        quantity: userInput.quantity,
+    }
+
+    db.update(DbUrl, TargetDb, "product", { "_id": new db.ObjectID(req.query.id) }, obj, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/product');
+        }
+    })
+
 })
 
 // 删除商品请求
-app.get('/product-delete', (req, res) => {
+app.get('/doproduct-delete/:id', (req, res) => {
+    let id = req.params.id;
 
-    res.send('删除商品');
+    db.delete(DbUrl, TargetDb, "product", { "_id": new db.ObjectID(id) }, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        res.redirect('/product');
+    })
 })
 
 // 监听端口
