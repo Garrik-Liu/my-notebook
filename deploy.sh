2bff8d907850d440d20285bc7c6b26cd72ab540b
Garrik-Liu <garrikliu@Gmail.com>
2019-02-21 15:14:53 +0800

    [docs](工程化/git/git面试题.md):add - git面试题.md
    开始写 git面试题.md

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ git push -f origin master
 # 提交相应目录下内容到对应分支
 git subtree push --prefix docs/.vuepress/dist origin gh-pages
 
-git subtree push --prefix notes_code origin notes_code
+git subtree push --prefix notes_code origin notes-code
--- docs/工程化/git/readme.md
+++ docs/工程化/git/readme.md
@@ -1,3 +1,4 @@
 # Git 笔记
 
 * [Git 攻略](./git攻略.md)
+* [Git 面试题](./git面试题.md)
