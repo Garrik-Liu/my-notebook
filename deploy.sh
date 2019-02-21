f5c3bd829e5c7996cf8d1299d9f469ca6176b27f
Garrik-Liu <garrikliu@Gmail.com>
2019-02-21 14:55:31 +0800

    [deploy](./deploy.sh): 修改了部署的方式

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ git push -f
 # 提交相应目录下内容到对应分支
 git subtree push -f --prefix docs/.vuepress/dist origin gh-pages
 
-git subtree push -f --prefix notes-code origin notes-code
+git subtree push -f --prefix notes_code origin notes-code
