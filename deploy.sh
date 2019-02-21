1168b5548f8f0d777fdf49d158b70e9040007696
Garrik-Liu <garrikliu@Gmail.com>
2019-02-21 15:07:58 +0800

    [docs](.):删除掉 master 分支是不需要的目录

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ git add .
 git commit 
 
 # 提交到 master 分支
-git push -f 
+git push -f origin master
 
 # 提交相应目录下内容到对应分支
 git subtree push --prefix docs/.vuepress/dist origin gh-pages
 
-git subtree push --prefix notes_code origin notes-code
+git subtree push --prefix notes_code origin notes_code
--- notes_code/express/简单身份验证/users.js
+++
@@ -1,7 +0,0 @@
-// 这个文件用于保存登录的账户信息
-
-module.exports = {
-    items: [
-        { username: "admin", password: "admin" },
-    ],
-}
