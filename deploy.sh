56b2b91f34b50a442828a3c2354f4f8d93d71ce8
Garrik-Liu <garrikliu@Gmail.com>
2019-12-16 01:53:34 -0500

    update - new content

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ git commit -m 'update - new content'
 git push -f origin master
 
 # 提交相应目录下内容到对应分支
-git subtree push -f --prefix=docs/.vuepress/dist origin gh-pages
+git subtree push --prefix=docs/.vuepress/dist origin gh-pages
 
-git subtree push -f --prefix=notes_code origin notes-code
+git subtree push --prefix=notes_code origin notes-code
