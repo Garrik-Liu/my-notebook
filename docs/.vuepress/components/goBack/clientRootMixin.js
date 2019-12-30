177c63acb329985821b3cdda4ec7219554eb8bea
Garrik-Liu <garrikliu@Gmail.com>
2019-12-30 13:31:42 -0500

    update - new content

--- docs/.vuepress/components/goBack/clientRootMixin.js
+++ docs/.vuepress/components/goBack/clientRootMixin.js
@@ -10,10 +10,13 @@ export default {
     },
     methods: {
         update() {
+            console.log(123);
             // 获取 sidebar
             const sidebar = document.getElementsByClassName('sidebar')[0];
+            console.log(sidebar);
             // 创建 go-back 组件
             const goBack = Vue.extend(goBack);
+            console.log(goBack);
             sidebar.insertBefore(goBack, sidebar.firstChild);
         }
     }
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,6 @@ module.exports = {
     title: 'Garrik 的笔记',
     description: '这里是 Garrik 的前端笔记本',
     base: '/my-notebook/',
-    theme: '@vuepress/theme-default',
     markdown: {
         lineNumbers: false,
     },
