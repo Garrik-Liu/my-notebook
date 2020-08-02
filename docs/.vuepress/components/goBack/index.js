3b89062e7368f9bef1c710c12804b73b6832cc00
Garrik-Liu <garrikliu@Gmail.com>
2020-08-02 14:45:00 -0400

    update - new content

--- docs/.vuepress/components/goBack/index.js
+++ docs/.vuepress/components/goBack/index.js
@@ -1,13 +1,7 @@
 const path = require("path");
-const mermaid = require("mermaid/dist/mermaid");
 
 module.exports = (options = {}, ctx) => {
-    mermaid.then(m => {
-        m.initialize({
-            startOnLoad: true
-        });
-        m.init();
-    });
+
     return {
         define: {},
         clientRootMixin: path.resolve(__dirname, "clientRootMixin.js"),
